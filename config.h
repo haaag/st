39708ee66cbbb01f884cbf2819b5718967ae9276
haaag <81921095+haaag@users.noreply.github.com>
2024-03-11 11:12:01 -0300

    chore: font and shell

--- config.h
+++ config.h
@@ -6,8 +6,8 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 // static char *font = "Iosevka Term:pixelsize=15:antialias=true:autohint=true";
-static char *font = "InputMono Nerd Font:pixelsize=16:antialias=true:autohint=true";
-static char *font2[] = { 
+static char *font = "Maple Mono NF:pixelsize=17:antialias=true:autohint=true";
+static char *font2[] = {
   "nonicons:pixelsize=12:antialias=true:autohint=true",
   "Material:size=12",
   "Noto Color Emoji:pixelsize=13:antialias=true:autohint=true",
@@ -26,7 +26,7 @@ static int borderpx = 0;
  * 4: value of shell in /etc/passwd
  * 5: value of shell in config.h
  */
-static char *shell = "/bin/sh";
+static char *shell = "/bin/zsh";
 char *utmp = NULL;
 char *stty_args = "stty raw pass8 nl -echo -iexten -cstopb 38400";
 
@@ -278,42 +278,40 @@ MouseKey mkeys[] = {
 // static char *copyoutput[] = { "/bin/sh", "-c", "st-copyout", "externalpipe", NULL };
 
 static Shortcut shortcuts[] = {
-  /* mask			  keysym          function        argument */
-  { XK_ANY_MOD,           	  XK_Break,       sendbreak,      {.i =  0} },
-  { ControlMask,          	  XK_Print,       toggleprinter,  {.i =  0} },
-  { ShiftMask,            	  XK_Print,       printscreen,    {.i =  0} },
-  { XK_ANY_MOD,           	  XK_Print,       printsel,       {.i =  0} },
-  { MODKEY,               	  XK_equal,       zoom,           {.f = +1} },
-  { MODKEY,               	  XK_minus,       zoom,           {.f = -1} },
-  { MODKEY,               	  XK_BackSpace,   zoomreset,      {.f =  0} },
-  { ControlMask | ShiftMask,  XK_C,           clipcopy,       {.i =  0} },
-  { ShiftMask,			          XK_Insert,      clippaste,      {.i =  0} },
-  { ControlMask | ShiftMask,  XK_V,           clippaste,      {.i =  0} },
-  { XK_ANY_MOD,			          Button2,	      selpaste,	      {.i =  0} },
-  { MODKEY,			              XK_Num_Lock,    numlock,        {.i =  0} },
-  { ControlMask | ShiftMask,  XK_U,           iso14755,       {.i =  0} },
-  { MODKEY,			              XK_Page_Up,     kscrollup,      {.i = -1} },
-  { MODKEY,               	  XK_Page_Down,   kscrolldown,    {.i = -1} },
-  // { ShiftMask,            XK_Page_Up,     kscrollup,      {.i = -1} },
-  // { ShiftMask,            XK_Page_Down,   kscrolldown,    {.i = -1} },
-  // { MODKEY,               XK_Up,          kscrollup,      {.i =  1} },
-  // { MODKEY,               XK_Down,        kscrolldown,    {.i =  1} },
-  // { MODKEY,               XK_k,           kscrollup,      {.i =  1} },
-  // { MODKEY,               XK_j,           kscrolldown,    {.i =  1} },
-  // { MODKEY,               XK_u,           kscrollup,      {.i = -1} },
-  // { MODKEY,               XK_d,           kscrolldown,    {.i = -1} },
-  // { MODKEY,               XK_m,		      changealpha,	  {.f = +2.00} },
-  { MODKEY,			              XK_s,		        changealpha,	  {.f = -0.05} },
-  { MODKEY,               	  XK_a,		        changealpha,	  {.f = +0.05} },
-  { TERMMOD,              	  XK_K,           zoom,           {.f = +1} },
-  { TERMMOD,              	  XK_J,           zoom,           {.f = -1} },
-  // { TERMMOD,              	  XK_Up,          zoom,           {.f = +1} },
-  // { TERMMOD,              	  XK_Down,        zoom,           {.f = -1} },
-  // { TERMMOD,              	  XK_U,           zoom,           {.f = +2} },
-  // { TERMMOD,              	  XK_D,           zoom,           {.f = -2} },
-  { MODKEY,                   XK_i,           copyurl,        {.i =  0} },
-  { TERMMOD,              	  XK_Return,      newterm,        {.i =  0} },
-	{ MODKEY,                   XK_c,           normalMode,     {.i =  0} },
+    /* mask			                keysym          function        argument */
+    { XK_ANY_MOD,           	    XK_Break,       sendbreak,      {.i =  0} },
+    { ControlMask,          	    XK_Print,       toggleprinter,  {.i =  0} },
+    { ShiftMask,            	    XK_Print,       printscreen,    {.i =  0} },
+    { XK_ANY_MOD,           	    XK_Print,       printsel,       {.i =  0} },
+    { MODKEY,               	    XK_equal,       zoom,           {.f = +1} },
+    { MODKEY,               	    XK_minus,       zoom,           {.f = -1} },
+    { MODKEY,               	    XK_BackSpace,   zoomreset,      {.f =  0} },
+    { ControlMask | ShiftMask,      XK_C,           clipcopy,       {.i =  0} },
+    { ShiftMask,			        XK_Insert,      clippaste,      {.i =  0} },
+    { ControlMask | ShiftMask,      XK_V,           clippaste,      {.i =  0} },
+    { XK_ANY_MOD,			        Button2,	    selpaste,	      {.i =  0} },
+    { MODKEY,			            XK_Num_Lock,    numlock,        {.i =  0} },
+    { ControlMask | ShiftMask,      XK_U,           iso14755,       {.i =  0} },
+    { MODKEY,			            XK_Page_Up,     kscrollup,      {.i = -1} },
+    { MODKEY,               	    XK_Page_Down,   kscrolldown,    {.i = -1} },
+    //
+    // { ShiftMask,                    XK_Page_Up,     kscrollup,      {.i = -1} },
+    // { ShiftMask,                    XK_Page_Down,   kscrolldown,    {.i = -1} },
+    // { MODKEY,                       XK_Up,          kscrollup,      {.i =  1} },
+    // { MODKEY,                       XK_Down,        kscrolldown,    {.i =  1} },
+    // { MODKEY,                       XK_k,           kscrollup,      {.i =  1} },
+    // { MODKEY,                       XK_j,           kscrolldown,    {.i =  1} },
+    // { MODKEY,                       XK_u,           kscrollup,      {.i = -1} },
+    // { MODKEY,                       XK_d,           kscrolldown,    {.i = -1} },
+    // { MODKEY,                       XK_m,		   changealpha,	  {.f = +2.00} },
+    //
+    { MODKEY,			            XK_s,           changealpha,	  {.f = -0.05} },
+    { MODKEY,               	    XK_a,           changealpha,	  {.f = +0.05} },
+    { TERMMOD,              	    XK_K,           zoom,           {.f = +1} },
+    { TERMMOD,              	    XK_J,           zoom,           {.f = -1} },
+    { MODKEY,                       XK_i,           copyurl,        {.i =  0} },
+    { TERMMOD,              	    XK_Return,      newterm,        {.i =  0} },
+    { MODKEY,                       XK_c,           normalMode,     {.i =  0} },
 
 };
 
